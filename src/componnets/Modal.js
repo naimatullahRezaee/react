0979071087c6326b0b1004350d3039adb6715083
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-05-30 13:35:13 +0200

    calcultor created

---
+++ src/componnets/Modal.js
@@ -0,0 +1,12 @@
+import "./modal.css";
+
+export default function Modal() {
+  return (
+    <div className="modal-backdrop">
+      <div className="modal">
+        <h2>10% off this Course</h2>
+        <p>Use the this Code [66#$@5678] for discount </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/componnets/title.js
@@ -0,0 +1,8 @@
+export default function Title() {
+  return (
+    <>
+      <h1 className="title">Calculators</h1>
+      <h2 className="subtitle">It calculate four options</h2>
+    </>
+  );
+}
